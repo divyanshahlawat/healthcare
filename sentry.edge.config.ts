33697cd01bc85662a488ea4a3bbd4858432819dc
Divyansh <divyanshahlawat035@gmail.com>
2024-09-16 20:14:04 +0530

    changes

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e0101da53f457d1b43491074ddc14991@o4507959098408960.ingest.de.sentry.io/4507959100702800",
+  // dsn: "https://e0101da53f457d1b43491074ddc14991@o4507959098408960.ingest.de.sentry.io/4507959100702800",
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
