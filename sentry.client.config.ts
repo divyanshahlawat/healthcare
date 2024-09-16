46a9a2c64b982546e923e99cb3683902c3670140
Divyansh <divyanshahlawat035@gmail.com>
2024-09-16 20:17:14 +0530

    SENTRY DISABLE

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,11 +2,11 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+// import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  // dsn: "https://e0101da53f457d1b43491074ddc14991@o4507959098408960.ingest.de.sentry.io/4507959100702800",
+// Sentry.init({
+//   dsn: "https://e0101da53f457d1b43491074ddc14991@o4507959098408960.ingest.de.sentry.io/4507959100702800",
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
+//   debug: false,
+// });
